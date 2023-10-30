b8bcde544424fabf2ec329097c66ae8a60cab5af
Avineash <avineash.m@gmail.com>
2023-10-30 15:09:13 +0800

    up8

---
+++ readfile.js
@@ -0,0 +1,44 @@
+function init(){
+
+const csvTable = document.getElementById("csvTable");
+
+// Specify the file name you want to read
+const csvFileName = "VIC_LGA_unemployment.csv";
+
+fetch(csvFileName)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch the CSV file.");
+    }
+    return response.text();
+  })
+  .then((csvText) => {
+    const csvData = d3.csvParse(csvText);
+
+    // Create the table header
+    const tableHeader = Object.keys(csvData[0]);
+    let tableHTML = '<thead><tr>';
+    tableHeader.forEach((header) => {
+      tableHTML += `<th>${header}</th>`;
+    });
+    tableHTML += '</tr></thead>';
+
+    // Create the table rows
+    tableHTML += '<tbody>';
+    csvData.forEach((row) => {
+      tableHTML += '<tr>';
+      tableHeader.forEach((header) => {
+        tableHTML += `<td>${row[header]}</td>`;
+      });
+      tableHTML += '</tr>';
+    });
+    tableHTML += '</tbody>';
+
+    // Set the table content
+    csvTable.innerHTML = tableHTML;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+j
+}window.onload = init
